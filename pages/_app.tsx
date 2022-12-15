d55b1c932bca1d06b0cf54a53f4f960e6c470cd1
lonelil <51315646+lonelil@users.noreply.github.com>
2022-12-16 06:52:45 +0800

    done ig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from "@next/font/local";
 
-const myFont = localFont({ src: "./Billy_2.0.ttf" });
+const billy = localFont({ src: "./Billy_2.0.ttf" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={myFont.className}>
+    <main className={`${billy.className} min-h-screen`}>
       <Component {...pageProps} />
     </main>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,7 +9,62 @@ export default function Home() {
         <meta name="description" content="lonelil's website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <h1 className="text-3xl font-bold underline">lonelil</h1>
+      <div className="py-28 grid grid-cols-12">
+        <div className="col-auto inline-grid"></div>
+        <div className="col-span-3 inline-grid">
+          <div className="mb-10">
+            <h1 className="text-6xl">lonelil</h1>
+            <p className="text-3xl mt-2">welcome to my website!</p>
+          </div>
+          <div>
+            <h1 className="text-4xl">my blog:</h1>
+            <div className="mt-5">
+              <div className="card w-96 bg-base-100 shadow-xl image-full">
+                <figure>
+                  <img src="" alt="Image" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Example Post</h2>
+                  <p>Example Post</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:col-span-6 lg:inline-grid hidden"></div>
+        <div className="lg:col-span-2 lg:inline-grid lg:items-center lg:justify-center hidden">
+          <div className="bg-gray-900 h-[40rem] w-24 flex flex-col items-center justify-center rounded-2xl">
+            <a
+              href="https://github.com/lonelil"
+              className="cursor-pointer"
+            >
+              <div className="relative flex justify-center items-centers">
+                <Image
+                  src="/icons/Github.webp"
+                  className="w-20 h-20 mt-[7.5px] mb-[7.5px] rounded-2xl"
+                  width="75"
+                  height="75"
+                  alt=""
+                />
+              </div>
+            </a>
+            <a
+              href="https://discord.com/users/603129750638034957"
+              className="cursor-pointer"
+            >
+              <div className="relative flex justify-center items-centers">
+                <Image
+                  src="/icons/Discord.webp"
+                  className="w-20 h-20 mt-[7.5px] mb-[7.5px] rounded-2xl"
+                  width="75"
+                  height="75"
+                  alt=""
+                />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
