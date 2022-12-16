535da5ae50adaaa509a097c6dc448032c4733128
lonelil <51315646+lonelil@users.noreply.github.com>
2022-12-17 05:21:48 +0800

    added cursor from https://github.com/alii/website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRef, useEffect } from "react";
 import localFont from "@next/font/local";
 import Head from "next/head";
 import { useLanyard } from "react-use-lanyard";
+import { loadCursor } from "../util/cursor";
 
 const billy = localFont({ src: "../fonts/Billy_2.0.ttf" });
 //@ts-ignore
@@ -11,6 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
     userId: "603129750638034957",
     socket: true,
   });
+  const ballCanvas = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window === "undefined" || !ballCanvas.current) {
+      return;
+    }
+
+    return loadCursor(ballCanvas.current);
+  }, []);
   return (
     <>
       <Head>
@@ -25,6 +35,10 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <main className={`${billy.className} min-h-screen`}>
         <Component {...pageProps} lonelil={lonelil} />
+        <div
+          ref={ballCanvas}
+          className="ball-transitions pointer-events-none fixed z-30 h-6 w-6 rounded-full border border-black bg-transparent opacity-0 shadow-md duration-200 dark:border-white"
+        />
       </main>
     </>
   );
