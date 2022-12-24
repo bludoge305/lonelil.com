b477abc26a83bfaaa4ad9df0c7d054414ca61049
lonelil <51315646+lonelil@users.noreply.github.com>
2022-12-24 21:08:22 +0800

    improve navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
+import { HiBars3 } from "react-icons/hi2";
+import { AiOutlineUser } from "react-icons/ai";
 
 export default function Navbar() {
+  const scrollPosition = useScrollPosition();
+
   return (
-    <div className="navbar bg-base-200">
+    <div
+      className={`navbar sticky top-0 z-50 transition-colors ${
+        scrollPosition > 0
+          ? "bg-opacity-60 bg-clip-padding shadow-none backdrop-blur-xl backdrop-filter"
+          : "bg-base-200 shadow"
+      }`}
+    >
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn-ghost btn-circle btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <HiBars3 size={30} />
           </label>
           <ul
             tabIndex={0}
@@ -29,13 +29,23 @@ export default function Navbar() {
             <li>
               <Link href={"/links"}>Links</Link>
             </li>
+            <li>
+              <Link href={"/projects"}>Projects</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
           </ul>
         </div>
       </div>
       <div className="navbar-center">
         <p className="text-xl normal-case">lonelil</p>
       </div>
-      <div className="navbar-end"></div>
+      <div className="navbar-end">
+        <Link href={"/links"} className="btn-ghost btn-circle btn">
+          <AiOutlineUser size={30} />
+        </Link>
+      </div>
     </div>
   );
 }
