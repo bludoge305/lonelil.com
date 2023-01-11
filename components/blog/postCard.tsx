a680f226127bca00baee0e401bb19934aa0a0b6a
lonelil <51315646+lonelil@users.noreply.github.com>
2023-01-11 16:28:04 +0800

    fix card height

--- components/blog/postCard.tsx
+++ components/blog/postCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function PostCard({ post }: any) {
   return (
     <div
-      className={`card mb-3 max-h-full overflow-hidden bg-primary shadow-xl ${
+      className={`card mb-3 h-full overflow-hidden bg-primary shadow-xl ${
         post.coverImage ? "image-full" : "h-full"
       }`}
     >
@@ -18,9 +18,7 @@ export default function PostCard({ post }: any) {
         </figure>
       ) : null}
       <div className="card-body max-h-full">
-        <h2 className="card-title">
-          {post.title}
-        </h2>
+        <h2 className="card-title">{post.title}</h2>
         <p
           style={{
             display: "-webkit-box",
